8125d2a13068299b28e609e5bfd97d01342284b3
vinicius emanuel <vncemanuel@gmail.com>
2018-10-16 18:31:03 -0300

    primeiro commit

--- ExemploTG_CEP/ViewController.swift
+++ ExemploTG_CEP/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import CEPSwift
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelX: UILabel!
+    @IBOutlet weak var labelY: UILabel!
+    @IBOutlet weak var phoneSideLabel: UILabel!
+    @IBOutlet weak var gameSideLabel: UILabel!
+    @IBOutlet weak var gameStatusLabel: UILabel!
+    
+    private let tiltLimitSup = 0.4
+    private let tiltLimitInf = -0.4
+    
+    private var sequence: [String] = ["Left","Right","Left","Dwon","Left"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.gameSideLabel.text = ""
+        self.phoneSideLabel.text = ""
+        self.gameStatusLabel.text = ""
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ExemploTG_CEP' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ExemploTG_CEP
+pod 'CEPSwift'
+
+end
